c15cfa6834ca486c7db7297dd557ffd0e5dd5211
MinhKhaNe <jacknguyen300804@gmail.com>
2026-01-26 10:12:42 +0700

    Create Bam_cell.v

---
+++ RTL/Multiplier/Bam_cell.v
@@ -0,0 +1,20 @@
+module Bam_cell(
+	input	wire	A,
+	input	wire	B,
+	input	wire	pre_OUT,
+	input	wire	Carry,
+
+	output	wire	Carry_out,
+	output	wire	OUT
+);
+
+  	wire			and_ab;
+	wire	[1:0]	result;
+
+	assign	and_ab 		= A & B;
+	assign	result		= and_ab + pre_OUT + Carry;	
+
+	assign	Carry_out	= result[1];
+	assign	OUT		= result[0];
+	
+endmodule
