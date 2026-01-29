bfd3eb0791b73c489eb56e2eacdff1a683111020
MinhKhaNe <jacknguyen300804@gmail.com>
2026-01-29 17:14:39 +0700

    Update Multiplier_log.v

--- RTL/Multiplier/Multiplier_log.v
+++ RTL/Multiplier/Multiplier_log.v
@@ -15,12 +15,21 @@ module Multiplier_log #(
 
 	output	wire	[WIDTH_MUL-1:0]	OUT
 );
+	function automatic integer clog2;
+        input integer value;
+        integer i;
+        begin
+            clog2 = 0;
+            for (i = value - 1; i > 0; i = i >> 1)
+                clog2 = clog2 + 1;
+        end
+    endfunction
 
-	parameter WIDTH_LOGA	= 	$clog2(WIDTH_A);
-	parameter WIDTH_LOGB	= 	$clog2(WIDTH_B);
+	parameter WIDTH_LOGA	= 	clog2(WIDTH_A);
+	parameter WIDTH_LOGB	= 	clog2(WIDTH_B);
 	parameter WIDTH_F		= 	`max(WIDTH_A, WIDTH_B);
 	parameter LOG_W			= 	`max(WIDTH_A + WIDTH_LOGA + 1, WIDTH_B + WIDTH_LOGB + 1);
-	parameter K_W			= 	`max(kWIDTH_LOGA, WIDTH_LOGB) + 1'b1;
+	parameter K_W			= 	`max(WIDTH_LOGA, WIDTH_LOGB) + 1'b1;
 	parameter X_W			= 	WIDTH_F - 1'b1;
 
 	wire			[WIDTH_LOGA-1:0]				ka;
@@ -35,13 +44,17 @@ module Multiplier_log #(
 	wire	signed	[WIDTH_A-1:0]					a_sign;
 	wire	signed	[WIDTH_B-1:0]					b_sign;
 	wire	signed	[WIDTH_MUL-1:0]					pro_sign;
-	wire											carry_low;
+	reg												carry_low;
 	wire			[LOG_W-1:0]						log_a;
 	wire			[LOG_W-1:0]						log_b;
-	wire			[LOG_W:0]						log_sum;
+	reg				[LOG_W:0]						log_sum;
 	
 	reg 	signed	[WIDTH_MUL-1:0]					product;
-	reg 	signed	[WIDTH_MUL-1:0]					out;
+	wire 			[X_W-1:0]     					xprod;
+	wire 			[LOG_W-X_W:0] 					kprod;
+	wire 			[WIDTH_A-1:0] 					abs_a;
+	wire 			[WIDTH_B-1:0] 					abs_b
+
 	
 	assign	a_sign 		= 	(~SIGNED) 		?	A :
 					 		(A[WIDTH_A-1]) ? {1'b0, -$signed(A[WIDTH_A-2:0])} : 
@@ -50,30 +63,33 @@ module Multiplier_log #(
 	assign	b_sign 		= 	(~SIGNED) 		?	B :
 					 		(B[WIDTH_B-1]) ? {1'b0, -$signed(B[WIDTH_B-2:0])} : 
 					 		{1'b0, $signed(B[WIDTH_B-2:0])};
+	
+	assign 	pro_sign 	= 	SIGNED & (A[WIDTH_A-1] ^ B[WIDTH_B-1]);
 
-	assign	pro_sign	= 	B[WIDTH_B-1] * A[WIDTH_A-1];
+	assign  abs_a 		= 	(SIGNED && A[WIDTH_A-1]) ? -A : A;
+	assign  abs_b		= 	(SIGNED && B[WIDTH_B-1]) ? -B : B;
 
-	lopd la #(
+	lopd #(
 		.WIDTH_I(WIDTH_A),
 		.WIDTH_L(WIDTH_LOGA)
-	)(
-		.in(A),
+	) la (
+		.in(abs_a), 
 		.out(ka)
 	);
 
-	lopd lb #(
+	lopd  #(
 		.WIDTH_I(WIDTH_B),
 		.WIDTH_L(WIDTH_LOGB)
-	)(
-		.in(B),
+	) lb (
+		.in(abs_b), 
 		.out(kb)
 	);
 	
-	assign 	shift_posA 	= WIDTH_A - ka;
-	assign 	shift_posB 	= WIDTH_B - kb;
+	assign 	shift_posA 	= WIDTH_A - ka - 1;
+	assign 	shift_posB 	= WIDTH_B - kb - 1;
 
-	assign 	shifted_A	= (A << shift_posA);
-	assign 	shifted_B	= (B << shift_posB);
+	assign shifted_A 	= (a_sign << shift_posA);
+	assign shifted_B 	= (b_sign << shift_posB);
 
 	assign	xa			= shifted_A[WIDTH_A-1:1];
 	assign	xb			= shifted_B[WIDTH_B-1:1];
@@ -101,17 +117,13 @@ module Multiplier_log #(
 
 	always @(*) begin
 		if(kprod > X_W) begin
-			product	<= (xprod << (kprod - X_W));
+			product = ( {1'b1, xprod} << (kprod - X_W));
 		end
 		else begin
-			product	<= (xprod >> (X_W - kprod));
+			product = ( {1'b1, xprod} >> (X_W - kprod));
 		end
-
-		product[kprod] = 1'b1;
 	end
 
-	assign	OUT = 	(~SIGNED) ? product :
-				  	(pro_sign) ? -$signed(product) :
-					product;
+	assign OUT = (SIGNED && (A[WIDTH_A-1] ^ B[WIDTH_B-1])) ? -product : product;
 endmodule
 
