c75b52507cf21efc8d3d77ac2fd3b8c019d5b879
Nguyen Minh Kha <jacknguyen300804@gmail.com>
2026-02-12 09:44:36 +0700

    Update Multiplier_booth.v

--- RTL/Multiplier/Multiplier_booth.v
+++ RTL/Multiplier/Multiplier_booth.v
@@ -2,12 +2,12 @@
 
 module Multiplier_booth #(
 	parameter APPROX_TYPE	= 0,
-	parameter APPROX_W		= 16,
+	parameter APPROX_W		= 16,			//Number of bit will approximate
 	parameter WIDTH_A 		= 16,
 	parameter WIDTH_B 		= 16,
 	parameter WIDTH_MUL 	= WIDTH_A + WIDTH_B,
 	parameter SIGNED 		= 0,
-	parameter STAGE			= 0
+	parameter STAGE			= 0				//Number of pipeline stage
 )(
 	input	wire					clk,
 	input	wire					rst_n,
@@ -30,12 +30,15 @@ module Multiplier_booth #(
 	reg 	signed 	[WIDTH_MUL-1:0] 	OUT_final;
 	reg		signed	[WIDTH_MUL-1:0]		product;
 	reg				[5:0]				i;
-	reg 	signed 	[WIDTH_MUL-1:0] 	pipe_reg [0:STAGE];
+	reg 	signed 	[WIDTH_MUL-1:0] 	pipe_reg [0:STAGE];		//Stimulate pipeline
 	integer 							p;
 
+	//Take 3 bits from B to calculate and then shift to higher and continue taking bits
 	assign 	Q = 	B_ext[2*i +: 3];
 
+	//Sign extend if SIGNED = 1
 	assign	A_ext 	= SIGNED ? $signed({{WIDTH_B{A[WIDTH_A-1]}} , A}) : $signed({{WIDTH_B{1'b0}}, A});	
+	//If SIGNED, add 1 bit 0 to LSB and 2 bit SIGN of B to MSB, if not add 1 bit 0 to LSB and 2 bit 0 to MSB
 	assign 	B_ext 	= SIGNED ? {{2{B[WIDTH_B-1]}}, B, 1'b0} : {2'b0, B, 1'b0};	
 	assign	done	= done_reg;
 
@@ -48,18 +51,18 @@ module Multiplier_booth #(
 		else if (pip_en) begin
 			if(i<cnt) begin
 				case(Q)
-					3'b000: product	<= product;
-					3'b001: product	<= product + (A_ext << (2*i));
-					3'b010: product	<= product + (A_ext << (2*i));
-					3'b011: product	<= product + (A_ext << (2*i+1));
-					3'b100: product	<= product - (A_ext << (2*i+1));
-					3'b101: product	<= product - (A_ext << (2*i));
-					3'b110: product	<= product - (A_ext << (2*i));
-					3'b111: product	<= product;
+					3'b000: product	<= product;							//Keep product
+					3'b001: product	<= product + (A_ext << (2*i));		//product = product + 1 * A
+					3'b010: product	<= product + (A_ext << (2*i));		//product = product + 1 * A
+					3'b011: product	<= product + (A_ext << (2*i+1));	//product = product + 2 * A
+					3'b100: product	<= product - (A_ext << (2*i+1));	//product = product - 2 * A
+					3'b101: product	<= product - (A_ext << (2*i));		//product = product - 2 * A
+					3'b110: product	<= product - (A_ext << (2*i));		//product = product - 2 * A
+					3'b111: product	<= product;							//Keep product
 				endcase
-				i <= i + 1'b1;
-				if(i == (cnt -1))
-					done_reg	<= 1'b1;
+				i <= i + 1'b1;		//Increase i to continue loop
+				if(i == (cnt -1))	//Finish all counting (0 -> (cnt-1))
+					done_reg	<= 1'b1;	//done_reg signal high
 				else 
 					done_reg	<= 1'b0;
 			end
@@ -74,20 +77,21 @@ module Multiplier_booth #(
 	always @(posedge clk or negedge rst_n) begin
 		if (!rst_n) begin
        		for(p = 0; p <= STAGE; p = p + 1) begin
-				pipe_reg[p]		<= {WIDTH_MUL{1'b0}};
+				pipe_reg[p]		<= {WIDTH_MUL{1'b0}};	//Reset all pipeline stage
 			end
 		end
 		else if (pip_en && done) begin
     		if(APPROX_TYPE)
+				//Trans approximate bit to 0 to reduce power
 				pipe_reg[0] <= {product[WIDTH_MUL-1:APPROX_W], {APPROX_W{1'b0}}};
 			else
 				pipe_reg[0]	<= product;
 
     		for (p = 1; p <= STAGE; p = p + 1) begin
-        		pipe_reg[p] <= pipe_reg[p-1];
+        		pipe_reg[p] <= pipe_reg[p-1];			//Stimulate pipeline stage
 			end
 		end
 	end
 
-	assign OUT = pipe_reg[STAGE];
+	assign OUT = pipe_reg[STAGE];						//OUT result when finish all pipeline stages
 endmodule
