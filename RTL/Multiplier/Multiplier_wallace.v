7c9470d2df1deddae7637c531d4640241ef22441
Nguyen Minh Kha <jacknguyen300804@gmail.com>
2026-02-23 10:06:46 +0700

    Update Multiplier_wallace.v

--- RTL/Multiplier/Multiplier_wallace.v
+++ RTL/Multiplier/Multiplier_wallace.v
@@ -1,11 +1,11 @@
 module Multiplier_wallace #(
-    parameter APPROX	= 0,
-	parameter APPROX_W	= 16,
-	parameter WIDTH_A 	= 16,
-	parameter WIDTH_B 	= 16,
-	parameter WIDTH_MUL = 32,
-	parameter SIGNED	= 0, 
-	parameter STAGE		= 0
+    parameter APPROX_TYPE	= 0,
+	parameter APPROX_W		= 16,
+	parameter WIDTH_A 		= 16,
+	parameter WIDTH_B 		= 16,
+	parameter WIDTH_MUL 	= 32,
+	parameter SIGNED		= 0, 
+	parameter STAGE			= 0
 )(
     input	wire 					clk,
 	input	wire					rst_n,
@@ -30,32 +30,34 @@ module Multiplier_wallace #(
 	assign	b_sign		= B[WIDTH_B-1];		//1st bit of B
 	assign 	pro_sign	= a_sign ^ b_sign;
 
-	assign 	a_abs	= 	(!SIGNED) 	? 	A :
-						(a_sign)	? 	(0-A) :
+	assign 	a_abs	= 	(!SIGNED) 	? 	A :			//Unsigned mode -> Keep A
+						(a_sign)	? 	(0-A) :		//Signed mode -> Take 2's complement
 						A;
 	
-	assign 	b_abs	= 	(!SIGNED) 	? 	B :
-						(b_sign)	? 	(0-B) :
+	assign 	b_abs	= 	(!SIGNED) 	? 	B :			//Unsigned mode -> Keep B
+						(b_sign)	? 	(0-B) :		//Signed mode -> Take 2's complement
 						B;
 
-	assign	a_wall	=	{1'b0, a_abs[WIDTH_A-2:0]};
+	assign	a_wall	=	{1'b0, a_abs[WIDTH_A-2:0]};	//MSB forced = 0 to avoid sign-extension
 	assign	b_wall	=	{1'b0, b_abs[WIDTH_B-2:0]};
 
+	//Wallace Tree Multiplier
 	Wallace_16bit #(
-		.APPROX(APPROX)
+		.APPROX(APPROX_TYPE)
 	) w0 (
 		.A(a_wall),
 		.B(b_wall),
 		.OUT(product)
 	);
 
+	//Restore sign after multiplication
 	always @(posedge clk or negedge rst_n) begin
 		if(!rst_n) begin
 			product_out	<= {WIDTH_MUL{1'b0}};	
 		end
 		else begin
 			if(SIGNED && pro_sign)
-				product_out	<= (0 - product);
+				product_out	<= (0 - product);	//Negate result
 			else
 				product_out	<= product;
 		end
