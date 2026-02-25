07435708b046e493edb1f55922efd80c8756eaac
Nguyen Minh Kha <jacknguyen300804@gmail.com>
2026-02-25 10:19:22 +0700

    Update processing_element_is.v

--- RTL/processing_element_is.v
+++ RTL/processing_element_is.v
@@ -3,26 +3,26 @@ module processing_element_is #(                         //Input Sationary (Store
     parameter   WIDTH_B             = 16,               //Width of B
     parameter   WIDTH_MAC           = 48,               //Width of MAC
     parameter   WIDTH_T             = 2,                //Width of threshold
-    parameter   ZERO_GATING_MULT    = 1,                //
-    parameter   ZERO_GATING_ADD     = 1,                //
-    parameter   MM_APPROX           = 1,                //
-    parameter   M_APPROX            = 1,                //
-    parameter   AA_APPROX           = 1,                //
-    parameter   A_APPROX            = 1,                //
+    parameter   ZERO_GATING_MULT    = 1,                //Skip multiplier if zero
+    parameter   ZERO_GATING_ADD     = 1,                //Skip adder if zero
+    parameter   MM_APPROX           = 1,                //LSB bits of multiplier if having approximation
+    parameter   M_APPROX            = 1,                //MSB bits of multiplier if having approximation
+    parameter   AA_APPROX           = 1,                //LSB bits of adder if having approximation
+    parameter   A_APPROX            = 1,                //MSB bits of adder if having approximation
     parameter   MUL_TYPE            = 0,                //Choosing Multiplier
     parameter   ADD_TYPE            = 0,                //Choosing Adder
     parameter   STAGE               = 0,                //Number of Stage of pipeline
-    parameter   ARITHMETIC          = 0,
-    parameter   SIGNED              = 0,
+    parameter   ARITHMETIC          = 0,                //Choosing result between FMA or (multiplier and adder)
+    parameter   SIGNED              = 0,                
 
     parameter   INTERMEDIATE_PIPELINE_STAGE = 0
 
 )(
     input   wire                    clk,
     input   wire                    rst_n,
-    input   wire    [WIDTH_A-1:0]   act,                //activation 
-    input   wire    [WIDTH_B-1:0]   wei,                //weight
-    input   wire    [WIDTH_MAC-1:0] MAC_IN,             //transport value to next PE to push out
+    input   wire    [WIDTH_A-1:0]   act,                //activation input
+    input   wire    [WIDTH_B-1:0]   wei,                //weight input
+    input   wire    [WIDTH_MAC-1:0] MAC_IN,             //MAC input
 
     input   wire                    pipeline_en,        //stimulate pipeline
     input   wire                    reg_clear,          //Clear Registers
@@ -41,11 +41,15 @@ module processing_element_is #(                         //Input Sationary (Store
     output  wire    [WIDTH_MAC-1:0] MAC_out             //MAC out
 );
 
+    //Derived Parameters
+    //Enable zero detection if any gating enabled
     parameter   ZERO_DETECTION  = ZERO_GATING_ADD | ZERO_GATING_MULT;
+    //Multiplier output width
     parameter   MUL_W           = (ARITHMETIC==0)   ?   (WIDTH_A+WIDTH_B) : WIDTH_MAC;
 
-    reg     [WIDTH_A-1:0]           act_reg, act_out_reg;                   //Act go to PE first
-    wire    [WIDTH_MAC-1:0]         mac_out_fma, mac_value, mac_out_adder;         
+    //Internal Signals
+    reg     [WIDTH_A-1:0]           act_reg, act_out_reg;                       //Act go to PE first
+    wire    [WIDTH_MAC-1:0]         mac_out_fma, mac_value, mac_out_adder;             
     wire                            pipeline_in;
     wire                            Zero_detected;
     wire    [MUL_W-1:0]             mul_value;
@@ -53,20 +57,22 @@ module processing_element_is #(                         //Input Sationary (Store
     wire                            mac_is_valid, zero;
     reg     [STAGE:0]               pipe_valid;
     reg     [1:0]                   cell_pipe;
-    reg     [WIDTH_MAC-1:0]         mac_buffer;
+    reg     [WIDTH_MAC-1:0]         mac_buffer;                                 // Output buffer
+    //Pipeline registers for MAC and weight
     reg     [WIDTH_MAC-1:0]         pipe_mac [0:STAGE];   
     reg     [WIDTH_B-1:0]           pipe_wei [0:STAGE];
     integer                         i;
-    reg                             act_is_valid;
+    reg                             act_is_valid;                               //Activation already captured
 
-    assign  mul_mux_sel     = 1'b0;                                         //
-    assign  pipeline_in     = pipeline_en && cell_en;                       //Internal pipeline signal
-    assign  c_switch_out    = 1'b0;                                         //
-    assign  wei_out         = pipe_wei[STAGE];                                      //Push WEIGHT to next PE
-    assign  mac_is_valid    = pipe_valid[STAGE];
-    assign  zero            = ZERO_DETECTION    ?   Zero_detected : 1'b0;
-    assign  act_out         = act_out_reg;                                  //Push ACTIVATION to nex PE
+    assign  mul_mux_sel     = 1'b0;                                             
+    assign  pipeline_in     = pipeline_en && cell_en;                           //PE runs only when pipeline and cell enabled
+    assign  c_switch_out    = 1'b0;                                             
+    assign  wei_out         = pipe_wei[STAGE];                                  //Push WEIGHT to next PE
+    assign  mac_is_valid    = pipe_valid[STAGE];                                //Mac is ready to push when finish all pipeline stages
+    assign  zero            = ZERO_DETECTION    ?   Zero_detected : 1'b0;       //Zero gating enable
+    assign  act_out         = act_out_reg;                                      //Push ACTIVATION to nex PE
 
+    //Instantiate Zero detection module
     Zero_detection #(
         .WIDTH_A(WIDTH_A),
         .WIDTH_B(WIDTH_B),
@@ -94,8 +100,8 @@ module processing_element_is #(                         //Input Sationary (Store
             ) fma_i (
                 .i_clk		        (clk),
                 .i_rstn		        (rst_n && (!reg_clear)),
-                .i_a		        (act_reg),                   //after zero detection
-                .i_b		        (wei),                   //after zero detection
+                .i_a		        (act_reg),                   
+                .i_b		        (wei),                   
                 .i_c                (MAC_IN),                
                 .i_msel             (mul_mux_sel),
                 .i_pipeline_en      (pipeline_in),
@@ -103,6 +109,7 @@ module processing_element_is #(                         //Input Sationary (Store
             );
         end 
         else begin                                              //If ARITHMETIC is 0, choosing value from ADDER and MULTIPLIER
+            //Mac_in + Activation * Weight
             Adder_generic #(
                 .WIDTH_A(WIDTH_MAC),
                 .WIDTH_B(MUL_W),
@@ -118,6 +125,7 @@ module processing_element_is #(                         //Input Sationary (Store
                 .OUT(mac_out_adder)
             );
 
+            //Activation * Weight
             Multiplier_generic #(
                 .WIDTH_A(WIDTH_A),
                 .WIDTH_B(WIDTH_B),
@@ -156,7 +164,7 @@ module processing_element_is #(                         //Input Sationary (Store
 
     assign cell_out = cell_pipe[1] && act_is_valid;
 
-    //Push MAC value out
+    //MAC_out Buffer
     always @(posedge clk or negedge rst_n) begin
         if(!rst_n) begin
            mac_buffer  <= {WIDTH_MAC{1'b0}};
@@ -206,6 +214,7 @@ module processing_element_is #(                         //Input Sationary (Store
         end
     end
 
+    //Forward activation to next PE
     always @(posedge clk or negedge rst_n) begin
         if(!rst_n) begin
             act_out_reg        <= {WIDTH_A{1'b0}};
@@ -220,6 +229,7 @@ module processing_element_is #(                         //Input Sationary (Store
         end
     end
 
+    //Stimulate pipeline for MAC and WEIGHT
     always @(posedge clk or negedge rst_n) begin
         if(!rst_n) begin
             for(i = 0; i < STAGE + 1; i = i + 1) begin
