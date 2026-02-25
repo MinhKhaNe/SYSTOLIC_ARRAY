c9069f7f4ea42b0082a2a498c738b4e0bb9c8488
Nguyen Minh Kha <jacknguyen300804@gmail.com>
2026-02-25 09:28:49 +0700

    Update Bam_cell.v

--- RTL/Multiplier/Bam_cell.v
+++ RTL/Multiplier/Bam_cell.v
@@ -1,20 +1,25 @@
 module Bam_cell(
-	input	wire	A,
-	input	wire	B,
-	input	wire	pre_OUT,
-	input	wire	Carry,
+	input	wire	A,				//Multiplier bit
+	input	wire	B,				//Multiplicand bit
+	input	wire	pre_OUT,		//Previous partial sum
+	input	wire	Carry,			//Carry in
 
 	output	wire	Carry_out,
 	output	wire	OUT
 );
 
+	//Internal Signals
   	wire			and_ab;
 	wire	[1:0]	result;
 
+	//Generate partial product
 	assign	and_ab 		= A & B;
+	//Full-adder operation
 	assign	result		= and_ab + pre_OUT + Carry;	
 
+	//MSB = carry out
 	assign	Carry_out	= result[1];
+	//LSB = sum output
 	assign	OUT			= result[0];
 	
 endmodule
