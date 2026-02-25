5ce349452f95a59c92051314382f030631635689
Nguyen Minh Kha <jacknguyen300804@gmail.com>
2026-02-25 09:12:22 +0700

    Update Adder_ideal.v

--- RTL/Adder/Adder_ideal.v
+++ RTL/Adder/Adder_ideal.v
@@ -4,9 +4,9 @@ module Adder_ideal #(
 	parameter 	WIDTH_A = 16,
 	parameter 	WIDTH_B = 16
 )(
-	input	wire	[WIDTH_A-1:0]			A,
-	input	wire	[WIDTH_B-1:0]			B,
-	input	wire					Carry,
+	input	wire	[WIDTH_A-1:0]					A,
+	input	wire	[WIDTH_B-1:0]					B,
+	input	wire									Carry,
 
 	output	wire	[`max(WIDTH_A, WIDTH_B)-1:0]	OUT
 );
@@ -14,14 +14,19 @@ module Adder_ideal #(
 
 	wire	signed	[BITS-1:0]			a;
 	wire	signed	[BITS-1:0]			b;
-	wire	signed					carry;
+	wire	signed						carry;
 	wire	signed	[BITS-1:0]			sum;
 
 	//Sign Extend
 	assign	a = {{{BITS-WIDTH_A}{A[WIDTH_A-1]}}, A};	
 	assign	b = {{{BITS-WIDTH_B}{B[WIDTH_B-1]}}, B};	
+
+	//Carry in
 	assign	carry = Carry;
 
+	//Sum = A + B + Carry_in
 	assign 	sum = a + b + carry;
+
+	//Truncate internal result to output
 	assign	OUT = sum;
 endmodule
