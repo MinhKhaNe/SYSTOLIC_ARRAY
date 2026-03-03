9c5694ff9bbc1d9a4f7d9f747f429fe366399992
Nguyen Minh Kha <jacknguyen300804@gmail.com>
2026-03-03 11:12:44 +0700

    Update tb_adder_gear.sv

--- TB/Adder_tb/tb_adder_gear.sv
+++ TB/Adder_tb/tb_adder_gear.sv
@@ -1,16 +1,16 @@
 module tb_adder_gear;
 
-	localparam int R = 0;
-	localparam int P = 2;
-	localparam int WIDTH_A = 4;
-	localparam int WIDTH_B = 8;
-	localparam int BITS    = (WIDTH_A > WIDTH_B) ? WIDTH_A : WIDTH_B;
+	localparam int R 		= 4;
+	localparam int P 		= 4;
+	localparam int WIDTH_A 	= 8;
+	localparam int WIDTH_B 	= 12;
+	localparam int BITS    	= (WIDTH_A > WIDTH_B) ? WIDTH_A : WIDTH_B;
 
 	logic	[WIDTH_A-1:0] 	A;
 	logic	[WIDTH_B-1:0]	B;
-	logic 			Carry;
+	logic 					Carry;
 	
-	logic [BITS-1:0]	OUT;
+	logic 	[BITS-1:0]		OUT;
 
 	Adder_gear #(
 		.R(R),
@@ -28,47 +28,80 @@ module tb_adder_gear;
 		$display(
 			"A=%0d, B=%0d, Carry=%b, OUT=%0d", $signed(A), $signed(B), Carry, $signed(OUT)
 		);
+		$display(
+			"A=%b, B=%b, Carry=%b, OUT=%b", $signed(A), $signed(B), Carry, OUT
+		);
 	endtask
 
 	initial begin
-		$dumpfile("adder_ideal.vcd");   
-       		$dumpvars(0, tb_adder_ideal);   
+		$dumpfile("tb_adder_gear.vcd");   
+       	$dumpvars(0, tb_adder_gear);   
 		A = 8'sd5;
 		B = 12'sd3;
 		Carry = 0;
 		#1 display_result();
 
 		A = -8'sd5;
-        	B = 12'sd3;
-        	Carry = 0;
-        	#1 display_result();
+        B = 12'sd3;
+        Carry = 0;
+        #1 display_result();
 
-        	A = -8'sd5;
-        	B = -12'sd7;
-        	Carry = 0;
-        	#1 display_result();
+        A = -8'sd5;
+        B = -12'sd7;
+        Carry = 0;
+        #1 display_result();
 
-        	A = -8'sd1;
-        	B = 12'sd1;
-        	Carry = 0;
-        	#1 display_result();
+        A = -8'sd1;
+        B = 12'sd1;
+        Carry = 0;
+        #1 display_result();
 
-        	A = -8'sd1;
-        	B = 12'sd1;
-        	Carry = 1;
-        	#1 display_result();
+        A = -8'sd1;
+        B = 12'sd1;
+        Carry = 1;
+        #1 display_result();
 
-        	A = 8'sd127;
-        	B = 12'sd1;
-        	Carry = 0;
-        	#1 display_result();
+        A = 8'sd127;
+        B = 12'sd1;
+        Carry = 0;
+        #1 display_result();
 
-        	A = -8'sd128;
-        	B = -12'sd1;
-        	Carry = 0;
-        	#1 display_result();
+        A = -8'sd128;
+        B = -12'sd1;
+        Carry = 0;
+        #1 display_result();
 
-       		#1 $finish;
+		A = -8'sd5;
+        B = 12'sd5;
+        Carry = 0;
+        #1 display_result();
+
+        A = -8'sd5;
+        B = -12'sd5;
+        Carry = 0;
+        #1 display_result();
+
+        A = -8'sd2;
+        B = 12'sd1;
+        Carry = 0;
+        #1 display_result();
+
+        A = -8'sd1;
+        B = 12'sd0;
+        Carry = 1;
+        #1 display_result();
+
+        A = 8'sd42;
+        B = 12'sd45;
+        Carry = 0;
+        #1 display_result();
+
+        A = -8'sd128;
+        B = 12'sd128;
+        Carry = 0;
+        #1 display_result();
+
+       	#1 $finish;
 	end	
 
 endmodule
